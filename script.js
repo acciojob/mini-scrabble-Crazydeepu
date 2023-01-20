1028ef5ca035744119036250e016ef464665f8f9
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-01-20 21:41:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your code here
+let evaluatedText = document.getElementById("evaluatedText");
+        let letterCount = document.getElementById("letterCount");
+
+        evaluatedText.addEventListener("input",calculateLength)
+
+        function calculateLength() {
+           // console.log("Someone wrote something");
+            let word = evaluatedText.value;
+            let wordLength = word.length;
+            letterCount.innerHTML = wordLength;
+        }
